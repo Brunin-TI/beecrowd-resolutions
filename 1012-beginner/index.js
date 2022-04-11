90233676d561c5baecab60abb93fa9c00a19bcd7
Brunin-TI <bruno.araujo@viannasempre.com.br>
2022-04-10 22:06:41 -0300

    refac: 1012 beginner

--- 1012-beginner/index.js
+++ 1012-beginner/index.js
@@ -9,14 +9,14 @@ const calculateCircleArea = (C) => pi * Math.pow(C, 2);
 const calculateAreaTrapezoid = (A, B, C) => ((A + B) / 2) * C;
 const calculateSquareArea = (B) => Math.pow(B, 2);
 const calculateRectangleArea = (A, B) => A * B;
-const formatAnswer = `TRIANGULO: ${calculateTriangleArea(A, C).toFixed(
-  3
-)}\nCIRCULO: ${calculateCircleArea(C).toFixed(
-  3
-)}\nTRAPEZIO: ${calculateAreaTrapezoid(A, B, C).toFixed(
-  3
-)}\nQUADRADO: ${calculateSquareArea(B).toFixed(
-  3
-)}\nRETANGULO: ${calculateRectangleArea(A, B).toFixed(3)}`;
-const answer = formatAnswer;
+const triangleArea = calculateTriangleArea(A, C);
+const formatAnswer = (triangleArea) =>
+  `TRIANGULO: ${triangleArea.toFixed(3)}\nCIRCULO: ${calculateCircleArea(
+    C
+  ).toFixed(3)}\nTRAPEZIO: ${calculateAreaTrapezoid(A, B, C).toFixed(
+    3
+  )}\nQUADRADO: ${calculateSquareArea(B).toFixed(
+    3
+  )}\nRETANGULO: ${calculateRectangleArea(A, B).toFixed(3)}`;
+const answer = formatAnswer(triangleArea);
 console.log(answer);
