f1af93f360bd2213db97b528bd52448a5a8a3b83
BruninTI <bruno.araujo@viannasempre.com.br>
2022-06-14 11:14:23 -0300

    feat: create 1094-beginner

--- 1094-beginner/index.js
+++ 1094-beginner/index.js
@@ -1,52 +1,79 @@
+/* 
+Maria acabou de iniciar seu curso de graduação na faculdade de medicina e precisa de sua ajuda para organizar
+ os experimentos de um laboratório o qual ela é responsável. 
+ Ela quer saber no final do ano, quantas cobaias foram utilizadas no laboratório 
+ e o percentual de cada tipo de cobaia utilizada.
+
+Este laboratório em especial utiliza três tipos de cobaias: sapos, ratos e coelhos.
+Para obter estas informações, ela sabe exatamente o número de experimentos que foram realizados, o tipo de cobaia utilizada
+e a quantidade de cobaias utilizadas em cada experimento.
+
+Entrada
+A primeira linha de entrada contém um valor inteiro N que indica os vários casos de teste que vem a seguir. 
+Cada caso de teste contém um inteiro Quantia (1 ≤ Quantia ≤ 15) que representa a 
+quantidade de cobaias utilizadas e um caractere Tipo ('C', 'R' ou 'S'), indicando o tipo de cobaia (R:Rato S:Sapo C:Coelho).
+
+Saída
+Apresente o total de cobaias utilizadas, 
+o total de cada tipo de cobaia utilizada e 
+o percentual de cada uma em relação ao total de 
+cobaias utilizadas, sendo que o percentual deve ser apresentado com dois dígitos após o ponto.
+
+*/
 const filePath = __dirname.split("\\").pop() + "/stdin";
 const input = require("fs").readFileSync(filePath, "utf8");
 const lines = input.split("\n");
 
-const [...values] = lines;
-const totalGuineaPigs = values
-  .map((value) => Number(value.slice(0, -1)))
-  .reduce((acc, value) => acc + value, 0);
-const totalOfRabbits = values
-  .filter((value) => {
-    return value.slice(-1) === "C";
-  })
-  .map((value) => Number(value.slice(0, -1)))
-  .reduce((acc, value) => acc + value, 0);
-const totalOfMice = values
-  .filter((value) => {
-    return value.slice(-1) === "R";
-  })
-  .map((value) => Number(value.slice(0, -1)))
-  .reduce((acc, value) => acc + value, 0);
-const totalOfFrogs = values
-  .filter((value) => {
-    return value.slice(-1) === "S";
-  })
-  .map((value) => Number(value.slice(0, -1)))
-  .reduce((acc, value) => acc + value, 0);
-const calculatePercentage = (total, totalOfAnimals) => {
-  return (totalOfAnimals * 100) / total;
+const adaptParams = (lines) => {
+  const extractQuantity = (animalId) => {
+    return lines
+      .slice(1)
+      .filter((line) => line.includes(animalId))
+      .map((line) => Number(line.split(" ")[0]));
+  };
+  const rabbits = extractQuantity("C");
+  const frogs = extractQuantity("S");
+  const mice = extractQuantity("R");
+  return { rabbits, frogs, mice };
 };
 
-console.log(`Total: ${totalGuineaPigs} cobaias`);
-console.log(`Total de coelhos: ${totalOfRabbits}`);
-console.log(`Total de ratos: ${totalOfMice}`);
-console.log(`Total de sapos: ${totalOfFrogs}`);
-console.log(
-  `Percentual de coelhos: ${calculatePercentage(
-    totalGuineaPigs,
-    totalOfRabbits
-  ).toFixed(2)} %`
-);
-console.log(
-  `Percentual de ratos: ${calculatePercentage(
-    totalGuineaPigs,
-    totalOfMice
-  ).toFixed(2)} %`
-);
-console.log(
-  `Percentual de sapos: ${calculatePercentage(
-    totalGuineaPigs,
-    totalOfFrogs
-  ).toFixed(2)} %`
-);
+const resolution = (params) => {
+  const sumarize = (arr) => {
+    return arr.reduce((acc, curr) => {
+      return acc + curr;
+    }, 0);
+  };
+  const rabbitsSum = sumarize(params.rabbits);
+  const frogsSum = sumarize(params.frogs);
+  const miceSum = sumarize(params.mice);
+  const totalSum = rabbitsSum + frogsSum + miceSum;
+
+  const percentage = (total, part) => {
+    return (part / total) * 100;
+  };
+  const rabbitPercentage = percentage(totalSum, rabbitsSum);
+  const mousePercentage = percentage(totalSum, miceSum);
+  const frogPercentage = percentage(totalSum, frogsSum);
+  return {
+    rabbitsSum,
+    frogsSum,
+    miceSum,
+    totalSum,
+    rabbitPercentage,
+    mousePercentage,
+    frogPercentage,
+  };
+};
+const formatAnswer = (answer) => {
+  const logs = [
+    `Total: ${answer.totalSum} cobaias`,
+    `Total de coelhos: ${answer.rabbitsSum}`,
+    `Total de ratos: ${answer.miceSum}`,
+    `Total de sapos: ${answer.frogsSum}`,
+    `Percentual de coelhos: ${answer.rabbitPercentage.toFixed(2)} %`,
+    `Percentual de ratos: ${answer.mousePercentage.toFixed(2)} %`,
+    `Percentual de sapos: ${answer.frogPercentage.toFixed(2)} %`,
+  ];
+  return logs.join("\n");
+};
+console.log(formatAnswer(resolution(adaptParams(lines))));
